cb74c3558c34887d400151040255dd0a1b9085ac
Elys <elys.sanntos@gmail.com>
2021-01-15 10:55:33 -0300

    Aula 20210114
    exercícios:
    Ler, Armazenar e Exibir Lista De Nomes
    Ler, exibir mensagem para cores do semáforo

---
+++ 20200113-PrimeirosPassos/20210114_Exercicios/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _20210114_Exercicios
+{
+    class LerArmazenarExibirListaDeNomes
+    {
+        static void Main(string[] args)
+        {
+
+            //Declarando listas genéricas
+            List<string> nomes = new List<string>();
+            
+            //Lendo e armazenando os nomes
+            Console.WriteLine("Digite 1º nome para inserir na lista: ");
+            nomes.Add(Console.ReadLine());
+            Console.WriteLine("Digite o 2º nome para inserir na lista: ");
+            nomes.Add(Console.ReadLine());
+            Console.WriteLine("Digite o 3º nome para inserir na lista: ");
+            nomes.Add(Console.ReadLine());
+            Console.WriteLine("Digite o 4º nome para inserir na lista: ");
+            nomes.Add(Console.ReadLine());
+            Console.WriteLine("Digite o 5º nome para inserir na lista: ");
+            nomes.Add(Console.ReadLine());
+
+            //Ordena a lista
+            nomes.Sort();
+            Console.WriteLine("A lista possui: " + nomes.Count);
+
+            //Imprime cada item da lista
+            nomes.ForEach(i => Console.WriteLine(i));
+            
+        }
+    }
+}
---
+++ 20200113-PrimeirosPassos/Semafaro/SemaforoArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Semafaro
+{
+    class SemaforoArray
+    {
+        static void Main(string[] args)
+        {
+
+            var semafaro = new string[] { "Verde", "Vermelho", "Amarelo" };
+            Console.WriteLine(" =========== Podemos atravessar? =========== ");
+            Console.WriteLine();
+            Console.WriteLine("Digite [1]Verde | [2]Amarelo | [3]Vermelho");
+            Console.WriteLine();
+            int cor = int.Parse(Console.ReadLine());
+
+            if (cor == 1)
+            {
+                Console.WriteLine("ABERTO: Pode atravessar com segurança.");
+
+                if (cor == 2)
+
+                    Console.WriteLine("ATENÇÃO: Fique alerta ao atravessar");
+
+                if (cor == 3)
+
+                    Console.WriteLine("PARE: O sinal está fechado.");
+            }
+            else
+            if (cor <=0 || cor >3)
+            {
+                Console.WriteLine("Avalie sua escolha, o valor informado é Nulo.");
+
+            }
+#if DEBUG
+            Console.WriteLine(); //pula linha
+            Console.WriteLine("Tecle enter para fechar...");
+            Console.ReadLine();
+#endif
+
+
+        }
+    }
+}
+
+
+
+
+
+
+  
+
