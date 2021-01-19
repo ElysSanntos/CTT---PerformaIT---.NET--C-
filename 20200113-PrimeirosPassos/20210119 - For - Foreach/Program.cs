fe83ac2f0e11c8bade5c046af9cef003123b5c53
Elys <elys.sanntos@gmail.com>
2021-01-19 19:49:06 -0300

    (no branch): Auto stash before checking out "HEAD"
    Semafaro2 , 20210116, AprendendoSwitch: arquivo do projeto descarregado
    
    Inicio do exercio - Menino Ou menina?

---
+++ 20200113-PrimeirosPassos/20210119 - For - Foreach/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20210119___For___Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 20200113-PrimeirosPassos/20210119-MeninoOuMenina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20210119_MeninoOuMenina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
