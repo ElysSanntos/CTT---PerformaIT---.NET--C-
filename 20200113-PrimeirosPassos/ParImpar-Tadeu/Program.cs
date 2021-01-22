7f2ccc5b664b67be867fe775e9e4198181cadbf1
Elys <elys.sanntos@gmail.com>
2021-01-21 22:10:42 -0300

    Atualizações
    Código Par Impar - Tadeu

---
+++ 20200113-PrimeirosPassos/ParImpar-Tadeu/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Listpareimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int par = 2; par <= 20; par = par + 2)
+            {
+                Console.WriteLine("Números pares: " + par);
+            }
+            for (int impar = 1; impar <= 20; impar = impar + 2)
+            {
+                Console.WriteLine("Números Ímpares " + impar);
+            }
+
+        }
+    }
+}
