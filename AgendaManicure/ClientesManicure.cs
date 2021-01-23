c49169b8f17912a3ca8b82fbef2d04661296899c
Andressa Camargo <annghell@gmail.com>
2021-01-23 10:56:43 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaManicure/ClientesManicure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaManicure
+{
+    class ClientesManicure
+    {
+        public int CodCliente { get; set; }
+        public string NomeCliente { get; set; }
+        public string TelefoneCliente { get; set; }
+
+
+    }
+}
