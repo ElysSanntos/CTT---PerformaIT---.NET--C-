3346811a42634c7877cad5fbf29c62e2053178b6
Elys <elys.sanntos@gmail.com>
2021-01-26 13:51:08 -0300

    Aula 25-01-2021

---
+++ ShareRH_SalaoT2-master/SalaoT2.Dominio/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace SalaoT2.Dominio
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public string CEP { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+
+    }
+}
---
+++ ShareRH_SalaoT2-master/SalaoT2.Dominio/Funcionario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaoT2.Dominio
+{
+    class Funcionario
+    {
+    }
+}
