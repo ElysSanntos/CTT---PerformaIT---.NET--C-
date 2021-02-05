f3b046f6d6f578b6651e6ffd652489d81c42315a
Elys <elys.sanntos@gmail.com>
2021-02-05 19:26:20 -0300

    Primeiro Exercício - Retorna Idade

---
+++ RetornaIdade/RetornaIdade/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetornaIdade.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{idade}")]
+    
+        public string Idade(int idade, DateTime dtNascimento)
+        {
+
+
+            if (dtNascimento <= DateTime.Now)
+            {
+                idade = DateTime.Now.Year - dtNascimento.Year;
+                if (DateTime.Now.DayOfYear < dtNascimento.DayOfYear)
+                {
+                    idade -= 1;
+                }
+                int resultado = Convert.ToInt32(idade);
+                return resultado.ToString();
+            }
+            else
+                return ("Data inválida");
+        }
+    }
+}
--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -38,8 +38,8 @@ namespace WebApplication1.Controllers
 
         // pegar um obj especifico
 
-        [HttpGet("{id}")]
-        public WeatherForecast Get (int id)
+        [HttpGet("{id}")]// Atributo verbo que define o tipo de solicitação--ID: atributo da lista
+        public WeatherForecast Get (int id)//Retorna a classe
         {
             var rng = new Random();
             var lista = Enumerable.Range(1, 5).Select(index => new WeatherForecast
@@ -49,7 +49,30 @@ namespace WebApplication1.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             }).ToList();
 
-            return lista.ElementAt(id - 1);
+            return lista.ElementAt(id - 1);//-1: Indice 0, do elemento 
+        }
+
+        [HttpGet("teste/{nome}")]
+        public string Get(string nome)
+        {
+            if (nome.ToLower() == nome)
+                return "Olá, " + nome;
+            else
+                return "Não sei quem é você!" + nome;
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Post(int dias, int temperatura)
+        {
+            var rng = new Random();
+            var lista = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToList();
+            lista.Add(new WeatherForecast { Date = DateTime.Now.AddDays(dias), TemperatureC = temperatura });
+            return lista;
         }
     }
 }
