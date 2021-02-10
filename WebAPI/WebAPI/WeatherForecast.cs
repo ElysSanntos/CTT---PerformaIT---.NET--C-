df3b27ffc97c173a68c2fae69f3e0d7dc0de792e
Elys <elys.sanntos@gmail.com>
2021-02-10 11:05:20 -0300

    WebApplication

---
+++ WebAPI/WebAPI/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebAPI
+{//Todo o conteúdo desta classe, vira o GET que pegamos na web - json
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
