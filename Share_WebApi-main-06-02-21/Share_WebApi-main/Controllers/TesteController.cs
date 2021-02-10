1a489eb864126e4d86c40c53328ed88d235373ed
Elys <elys.sanntos@gmail.com>
2021-02-10 11:49:48 -0300

    zoneado

---
+++ Share_WebApi-main-06-02-21/Share_WebApi-main/Controllers/TesteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("feira")]
+    [ApiController]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet("banana")]
+        public string GetB()
+        {
+            return "olá banana!";
+        }
+
+        [HttpGet("goiaba")]
+        public string GetG()
+        {
+            return "olá goiaba!";
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "olá Pessoa!";
+        }
+
+        [HttpGet("{fruta}")]
+        public string GetF(string fruta)
+        {
+            return $"olá {fruta}!";
+        }
+    }
+}
